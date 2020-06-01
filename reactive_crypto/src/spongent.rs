8a21f25ea4ae4643d76289de6b22e556b70525ae
Gianluca Scopelliti <gianlu.1033@gmail.com>
2020-06-01 12:53:09 +0200

    associated data is not anymore part of the ciphertext

--- reactive_crypto/src/spongent.rs
+++ reactive_crypto/src/spongent.rs
@@ -14,11 +14,10 @@ pub fn encrypt(plaintext : &[u8], key : &[u8], data : &[u8]) -> Result<Vec<u8>, 
         return Err(Error::IllegalArguments)
     }
 
-    let mut ciphertext : Vec<u8> = Vec::with_capacity(pl_len + sancus_security + ad_len);
-    ciphertext.extend_from_slice(data);
+    let mut ciphertext : Vec<u8> = Vec::with_capacity(pl_len + sancus_security);
     ciphertext.extend_from_slice(plaintext);
 
-    let mac = match spongent_wrap(key, data, plaintext, &mut ciphertext[ad_len..], false) {
+    let mac = match spongent_wrap(key, data, plaintext, &mut ciphertext, false) {
         Ok(m) => m,
         Err(_) => return Err(Error::EncryptionError)
     };
@@ -33,30 +32,20 @@ pub fn decrypt(ciphertext : &[u8], key : &[u8], data : &[u8]) -> Result<Vec<u8>,
     let sancus_security = key.len();
     let ad_len = data.len();
 
-    let cipher_len = c_len - sancus_security - ad_len;
+    let cipher_len = c_len - sancus_security;
 
     //TODO: the decrypting function panics if plaintext and data are not multiple of 2 bytes
     if c_len % 2 != 0 || ad_len % 2 != 0 {
         return Err(Error::IllegalArguments)
     }
 
-    if c_len != ad_len + cipher_len + sancus_security {
-        return Err(Error::IllegalArguments)
-    }
-
-    let ad = &ciphertext[..ad_len];
-    let cipher = &ciphertext[ad_len..ad_len + cipher_len];
-    let mac = &ciphertext[ad_len + cipher_len..];
-
-    if ad != data {
-        // this to avoid potential replay attacks.
-        return Err(Error::IllegalArguments)
-    }
+    let cipher = &ciphertext[..cipher_len];
+    let mac = &ciphertext[cipher_len..];
 
     let mut plaintext : Vec<u8> = Vec::with_capacity(cipher_len);
     plaintext.extend_from_slice(cipher);
 
-    match spongent_unwrap(key, ad, cipher, mac, &mut plaintext) {
+    match spongent_unwrap(key, data, cipher, mac, &mut plaintext) {
         Ok(_) =>  Ok(plaintext),
         Err(e) => {
             println!("{:?}", e);
