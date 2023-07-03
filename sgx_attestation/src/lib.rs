63511e09aa41249b93d5e8673cbe87e1fa7d0805
Gianluca Scopelliti <gianluca.scopelliti@ericsson.com>
2023-07-03 14:24:23 +0200

    sgx_attestation: export mbedtls

--- sgx_attestation/src/lib.rs
+++ sgx_attestation/src/lib.rs
@@ -15,6 +15,7 @@ pub use enclave::*;
 pub use error::*;
 
 pub use sgx_crypto;
+pub use sgx_crypto::mbedtls as mbedtls;
 
 #[cfg(any(feature = "client", feature = "sp"))]
 const MAX_HOST_SIZE : usize = 256;
